46f99eebd17d59f9b49b853eab76fd8f15cc27fe
fred belotte <fredbelotte@outlook.com>
2020-05-07 00:58:07 -0500

    add latest unreleased changes to changelog [finishes #172045619]

--- jekyll/src/_scripts/sentry.script.js
+++ jekyll/src/_scripts/sentry.script.js
@@ -2,7 +2,7 @@ import * as Sentry from "@sentry/browser";
 
 Sentry.init({
   dsn:
-    "https://ce886c5d36cf4fed849158da6c7d1fb7@o388320.ingest.sentry.io/5226540",
+    "https://b99614d4459c4d2b9d660b0a0578d007@o388320.ingest.sentry.io/5226559",
 });
 
 export default Sentry;
