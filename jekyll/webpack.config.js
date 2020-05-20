c504462c08a2df3767fcdebe55db5b2e2d65f98a
fred belotte <fredbelotte@outlook.com>
2020-05-19 23:05:26 -0500

    rewrite build workflow [#172783138]

--- jekyll/webpack.config.js
+++ jekyll/webpack.config.js
@@ -1,22 +1,27 @@
 const path = require("path");
 const SentryPlugin = require("@sentry/webpack-plugin");
 
-module.exports = {
-  devtool: "source-map",
-  entry: "./src/_scripts/main.js",
-  mode: "production",
-  output: {
-    filename: "rvtr_app.bundle.js",
-    path: path.resolve(__dirname, "src/assets/js/"),
-  },
-  plugins: [
-    new SentryPlugin({
-      include: "./src/assets/js/",
-      release: `rvtr-app@${process.env.npm_package_version}`,
-      setCommits: {
-        auto: true,
-        repo: "rvtr-app",
-      },
-    }),
-  ],
+module.exports = (env) => {
+  const isDryRun = env && env.dry_run;
+
+  return {
+    devtool: "source-map",
+    entry: "./src/_scripts/main.js",
+    mode: "production",
+    output: {
+      filename: "rvtr_app.bundle.js",
+      path: path.resolve(__dirname, "src/assets/js/"),
+    },
+    plugins: [
+      new SentryPlugin({
+        dryRun: isDryRun,
+        include: "./src/assets/js/",
+        release: `rvtr-app@${process.env.npm_package_version}`,
+        setCommits: {
+          auto: true,
+          repo: "rvtr-app",
+        },
+      }),
+    ],
+  };
 };
